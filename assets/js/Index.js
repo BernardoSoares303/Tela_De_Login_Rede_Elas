b8ac8c6c0675dbe332e87deb85812b0f76e176e2
BernardoSoares303 <bbnado34@gmail.com>
2026-05-13 19:58:14 -0300

    comentarios no js

--- assets/js/Index.js
+++ assets/js/Index.js
@@ -7,11 +7,13 @@ function IrTelaLogin() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+    // EventListener Do botão de Fazer Cadastro
     const cadastro = document.querySelector("#CriarConta");
     cadastro.addEventListener("click", (e) => {
         IrTelaCadastro();
     })
 
+    // EventListener Do botão de Fazer Login
     const login = document.querySelector("#Login");
     login.addEventListener("click", (e) => {
         IrTelaLogin();
