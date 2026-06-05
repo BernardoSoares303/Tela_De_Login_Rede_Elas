8c0b179d0490520b956f8acc497e168ed8632da4
BernardoSoares303 <bbnado34@gmail.com>
2026-06-05 17:24:09 -0300

    Função de logout criada

--- assets/js/home.js
+++ assets/js/home.js
@@ -4,4 +4,15 @@ document.addEventListener("DOMContentLoaded", () => {
     if (usuario?.nivel == "admin") {
         document.getElementById("Painel__adm").style.display = "block";
     }
+
+    const logoutBtn = document.getElementById("Logout");
+    logoutBtn.addEventListener("click", (e) => {
+        logout();
+    })
 });
+
+
+function logout() {
+    localStorage.removeItem("usuarioLogado");
+    window.location.href = "index.html";
+}
