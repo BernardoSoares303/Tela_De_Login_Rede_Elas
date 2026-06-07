632ee878e02b78f90355eee0e6f107ebd711b077
BernardoSoares303 <bbnado34@gmail.com>
2026-06-07 15:15:23 -0300

    Criação de denuncias implementadas

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,8 +1,12 @@
+// Variável global para armazenar a postagem selecionada para denúncia
+let postagemSelecionada = null;
+
+// Puxa o Usuario Logado do localStorage para poder usar suas infmações, como o ID para criar denúncias e o nível para mostrar o painel do admin
+const usuario = JSON.parse(
+    localStorage.getItem("usuarioLogado")
+);
+
 document.addEventListener("DOMContentLoaded", () => {
-    
-    const usuario = JSON.parse(
-        localStorage.getItem("usuarioLogado")
-    );
 
     // Carregamento do Botão de Painel do Admin
     if (usuario?.nivel == "admin") {
@@ -19,8 +23,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const denunciarBtns = document.querySelectorAll(".relato-card__report");
     denunciarBtns.forEach((botao) => {
         botao.addEventListener("click", () => {
+            const relatoCard = botao.closest(".relato-card");
+            postagemSelecionada = Number(relatoCard.dataset.id);
             abrirModalDenuncia();
         });
+    });
+
+    // Enviar denúncia
+    const enviarDenunciaBtn = document.querySelector(".report-modal__button");
+    enviarDenunciaBtn.addEventListener("click", () => {
+        criarDenuncia();
     });
 
     // FECHAR MODAL
@@ -43,4 +55,60 @@ function abrirModalDenuncia() {
 function fecharModalDenuncia() {
     const modal = document.getElementById("reportModal");
     modal.style.display = "none";
+}
+
+function criarDenuncia() {
+
+    // pega textarea do modal
+
+    const textarea = document.querySelector(
+        ".report-modal__textarea"
+    );
+
+    // pega motivo digitado
+    const motivo = textarea.value;
+
+    // valida campo vazio
+    if (!motivo.trim()) {
+        alert("Digite o motivo da denúncia");
+        return;
+    }
+
+    // pega denúncias existentes
+    const denuncias = JSON.parse(
+        localStorage.getItem("denuncias")
+    ) || [];
+
+    // gera ID automático
+    const ultimoId =
+        denuncias.length > 0
+            ? denuncias[denuncias.length - 1].id
+            : 0;
+
+    // cria denúncia
+    const novaDenuncia = {
+        id: ultimoId + 1,
+        postagemId: postagemSelecionada,
+        usuarioId: usuario.id,
+        descricao: motivo,
+        dataCriacao: new Date().toISOString(),
+        status: "pendente"
+    };
+
+    // adiciona no array
+    denuncias.push(novaDenuncia);
+
+    // salva no localStorage
+    localStorage.setItem(
+        "denuncias",
+        JSON.stringify(denuncias)
+    );
+
+    // limpa textarea
+    textarea.value = "";
+
+    fecharModalDenuncia();
+
+    window.alert("Denúncia criada");
+
 }
